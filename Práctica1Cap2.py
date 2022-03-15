2e4c76d14fb5392c8c637ba803fbab96f765e20c
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 00:30:07 -0600

    hice practica 1
    p1

---
+++ Práctica1Cap2.py
@@ -0,0 +1,10 @@
+###############-AREA DE PRUEBA-###################
+#mensaje = "hola gg"
+#mensaje = 100
+#print(mensaje + 200)
+###############-AREA DE PRUEBA-###################
+mensaje = "hola qpd"#asignamos a mensaje el string
+v1 = 5#asignamos el primer valor entero a v1
+v2 = 3#asignamos el valor entero a v2
+res = v1+v2#asignamos la suma de v1 y v2 a res
+print(res)#imprimimos y mandamos como argumento el resultado de la variable res
