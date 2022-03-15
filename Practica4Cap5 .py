7b531624a79ea96bce9a71ca0d46d1b615f95cc6
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 01:09:49 -0600

    Hice pratica 4
    funciones upper,lower, y title

---
+++ Practica4Cap5 .py
@@ -0,0 +1,15 @@
+###############-AREA DE PRUEBA-###################
+PrimeraPalabra = "progra"
+PrimeraPalabra = PrimeraPalabra.title()#pone en mayuscula primera y despues de un espacio
+#SgundaPalabra = "facil"
+#nombre = PrimeraPalabra + " " + SgundaPalabra + "."
+#upper sirve para convertir todo a mayusculas
+#lower sirve para convertir todo a minusculas
+#print(PrimeraPalabra)
+###############-AREA DE PRUEBA-###################
+nombre = "enrique barros fernández"
+print(nombre.title())
+n2 = "Esta Es Una Frase Para Ser Formateada."
+print(n2.lower())
+n3 = "Esta Es Una Frase Para Ser Formateada."
+print(n3.upper())
