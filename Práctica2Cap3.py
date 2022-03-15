a2278a3117fa8749e2808119db6c1c7745e31d59
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 00:42:21 -0600

    Hice practica 2
    practica 2 de comillas y comillas simples

---
+++ Práctica2Cap3.py
@@ -0,0 +1,8 @@
+###############-AREA DE PRUEBA-###################
+#mensaje = 'para poner comillas dentro, es necesario poner comillas "simples" desde afuera, y viceversa'
+#print(mensaje)
+###############-AREA DE PRUEBA-###################
+m1 ="esta frase 'prueba'"#almacena en m1 la frase entre comillas simples
+m2 ='esta frase "prueba"'#almacena en m2 la frase entre comillas
+print(m1)#imprime m1
+print(m2)#imprime m2
