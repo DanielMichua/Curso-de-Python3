fb8bfcbcb10df0f9cbd98cedec79de7d62d6af2e
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 02:01:37 -0600

    practica 11
    eliminar una posicion especifica en listas

---
+++ Practica11Cap12.py
@@ -0,0 +1,7 @@
+#para eliminar una posicion especifica, se usa del lista[posicion deseada]
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#se declara la lista
+del colores[1]#deseamos elminiar azul, en la posicion 1
+del colores[4]#deseamos elminiar marron, en la posicion 4
+del colores[-4]#deseamos eliminar negro en posicion -4
+del colores[-3]#por ultimo, deseamos elminiar rosa en la posicion -3
+print(colores)#imprimimos la lista completa y vemos que ya no aparecen los elementos que eliminamos
