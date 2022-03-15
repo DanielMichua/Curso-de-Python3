6c3934f99b970d57f22df5e1f18a952b6045e94a
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 01:17:33 -0600

    Practica 5
    Saltos de linea

--- Practica4Cap5 .py
+++ Practica4Cap5 .py
@@ -7,9 +7,9 @@ PrimeraPalabra = PrimeraPalabra.title()#pone en mayuscula primera y despues de u
 #lower sirve para convertir todo a minusculas
 #print(PrimeraPalabra)
 ###############-AREA DE PRUEBA-###################
-nombre = "enrique barros fernández"
-print(nombre.title())
-n2 = "Esta Es Una Frase Para Ser Formateada."
-print(n2.lower())
-n3 = "Esta Es Una Frase Para Ser Formateada."
-print(n3.upper())
+nombre = "enrique barros fernández"#primero declaramos la variable string
+print(nombre.title())#despues imprimimos la variable anteriormente declarada, pero con la funcion title para que muestre la primer mayuscula de cada palabra
+n2 = "Esta Es Una Frase Para Ser Formateada."#declaramos v2
+print(n2.lower())#imprimimos la variable 2, pero con la funcion lower para que la haga toda a minusculas
+n3 = "Esta Es Una Frase Para Ser Formateada."#declaramos la variable 3
+print(n3.upper())#usamos la funcion upper para convertir toda a mayusculas
