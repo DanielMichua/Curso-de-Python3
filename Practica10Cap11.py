d6514b4eb16821b6c11c871c56a4e87fc442fb1f
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 01:54:58 -0600

    Practica 10
    listas negativas

---
+++ Practica10Cap11.py
@@ -0,0 +1,3 @@
+#lista[-1], para llamar al ultimo elemento de la lista, -2 para llamar al penultimo
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores[-1],colores[-7],colores[-5],colores[-2],colores[-10])#accedo a la posicion deseada yendo desde atras hacia adelante (considerando la ultima posicion como -1, y la primera como -10)
