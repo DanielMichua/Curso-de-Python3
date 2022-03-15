09f61c87b736494dd2829d6d8a15df0206069a3c
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 01:49:03 -0600

    Practica 9
    Listas

---
+++ Practica9Cap10.py
@@ -0,0 +1,5 @@
+#lista = array, coleccion de elementos
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[0],colores[7])#imprime el rojo y rosa, que esa en la posicion 0 y 7
+numeros = ["tres","dos","cinco","cuatro","uno"]#aqui se ve la posicion de acuerdo a lo pedido
+
