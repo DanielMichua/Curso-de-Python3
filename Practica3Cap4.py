9e8d858c8dc2eb53155c943732270fc65f12217c
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 00:57:53 -0600

    Practica 3
    Hice practica 3

---
+++ Practica3Cap4.py
@@ -0,0 +1,19 @@
+###############-AREA DE PRUEBA-###################
+#PrimeraPalabra = 'progra'
+#SgundaPalabra = "facil"
+#nombre = PrimeraPalabra + " " + SgundaPalabra + "."
+#print(nombre)
+###############-AREA DE PRUEBA-###################
+PrimeraPalabra = 'progra'#asigno variable1
+SegundaPalabra = "facil"#asigno variable2
+nombre = PrimeraPalabra + " " + SegundaPalabra + "."#concateno variable 1 y variable2
+print(nombre)#imprimo la concatenacion de ambas variables
+nombre2 = "hola"+"que tal"#concateno strings directamente en una variable
+print(nombre2)#imprimo dicha variable
+print("string1"+" "+"string2")#concateno directamente desde la funcion print 2 strings
+nom1 = "Daniel "#declaro nombre1
+nom2 = "Alejandro "#declaro nombre2
+ape1 = "Michua "#declaro mi primer apellido
+ape2 = "Alvarez"#declaro mi segundo apellido
+nomc = nom1+nom2+ape1+ape2#concateno mi nombre completo
+print(nomc)#e imprimo
