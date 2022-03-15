731c40632a95fbde7d74e1b769dccc69b76adf4e
DanielMichua <danielmichua3005@gmail.com>
2022-03-15 01:30:53 -0600

    Practica 6
    Operadores matematicos

---
+++ Practica6Cap7.py
@@ -0,0 +1,7 @@
+#para concatenar operaciones matematicas se usa la coma en vez de la +
+suma = 20 + 23 + 44#para hacer la suma hacemos lo siguiente
+resta = -44 + 20 + 23#para hacer la resta hacemos lo siguiente
+mul = 435*2#para la multiplicacion hacemos el uso de *
+div = 20/2#para la diision /
+op =10+5-15/17#aqui usamos varios operadores
+print(suma,resta,mul,div,op)#y mostramos separando por coma
