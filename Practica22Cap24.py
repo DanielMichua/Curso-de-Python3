5eba556cb8a4fd75529d04914cca3de54d56c08b
DanielMichua <danielmichua3005@gmail.com>
2022-03-16 23:32:24 -0600

    22-23
    practicas 22-23

--- Practica22Cap24.py
+++ Practica22Cap24.py
@@ -1,6 +1,14 @@
-entrada = input("introduce el nombre del color: \n")#declaramos para la entrada de info del usuario
-colores = ("verde","azul","negro","gris","rojo")#declaramos la tupla
-if entrada in colores:#primera condicion. si esta en la lista lo que escribio el usuario, imprime lo que esta dentro del if
-    print("el color esta en la lista")
-else:#y si no está, muestra el else
-    print("el color no esta en la lista :(")
+x = 0
+y = 0
+z = 10
+while x!=15:
+    x=x+5
+    print(x)
+
+while y !=-100:
+    y = y-20
+    print(y)
+
+while z != 0:
+    z=z-1
+    print("El valor del bucle es: ",z)
---
+++ Practica23Cap27.py
@@ -0,0 +1,14 @@
+x = 0#declaramos las variables
+y = 0
+z = 10
+while x!=15:#para el primer caso, se va a ejecutar el while mientras sea diferente de 15, con aumento de 5, y hasta que sea 15 va a salir
+    x=x+5
+    print(x)
+
+while y !=-100:#en este caso, se va a efectuar desde 0 hasta -100, con saltos de 20, hasta -100 se va a salir del bucle
+    y = y-20
+    print(y)
+
+while z != 0:#aqui se inicializa en 10, y decrece en 1, y por cada ciclo se muestra la posicion en el buqle
+    z=z-1
+    print("El valor del bucle es: ",z)
